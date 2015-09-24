a5c852f636571eda0f2ec6276b714f3082c0d262
dsun <dsun386@gmail.com>
2015-09-25 00:02:22 +0300

    first commit

---
+++ com/dsun/run/Category.java
@@ -0,0 +1,13 @@
+package com.dsun.run;
+
+public class Category {
+	String name;
+	
+	
+	public Category(String a){
+		name = a;
+		
+	}
+	
+	 
+}
---
+++ com/dsun/run/Run.java
@@ -0,0 +1,19 @@
+package com.dsun.run;
+
+
+
+public class Run {
+
+	public static void main(String[] args) {
+		String s = "smth";
+		Category cat1;
+//		System.out.println(cat1.name);
+		numbers(2, 5);
+	}
+
+	static void numbers(int i1, int i2){
+
+		int sum = i1 + i2;
+		System.out.println(sum);
+	}
+}
